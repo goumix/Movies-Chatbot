98633ea2d5eafdd2bdfd5a2954fb3a098f3f5e10
Laetitia <laetitia.granjon@epitech.digital>
2024-01-30 10:14:39 +0100

    app du chatbot

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 # from langchain_community.document_loaders import LocalDocumentLoader
+import langchain
+from sqlalchemy.engine import URL
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+import watson
+import sys
+sys.path.append("/opt/anaconda3/lib/python3.9/site-packages")exit 
 import streamlit as st
 
 # Give app title
-st.title('Chatbot')
+st.title('On regarde quoi ce soir ?')
+
+# Setup a session state message variable to hold all the old messages 
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+#Display all the historical messages
+for message in st.session_state.messages:
+    st.chat_message(message['role']).markdown(message['content'])
 
 # Build a prompt input template to display the prompt
 prompt = st.chat_input('Enter your prompt here')
@@ -16,3 +28,7 @@ prompt = st.chat_input('Enter your prompt here')
 if prompt: 
     #display the prompt
     st.chat_message('user').markdown(prompt)
+    #store the user prompt in state
+    st.session_state.messages.append({'role':'user', 'content':prompt})
+
+
